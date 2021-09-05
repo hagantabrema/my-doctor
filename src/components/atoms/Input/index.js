6002690d0e20823324404e1e37dba1d5f7c593ce
hagantabrema <hagantabangun@gmail.com>
2021-09-05 18:48:18 +0700

    custom useState, onFocus, onBlur

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,31 +1,47 @@
-import React from 'react'
-import { StyleSheet, Text, TextInput, View } from 'react-native'
-import { colors } from '../../../utils/colors'
-import { fonts } from '../../../utils/fonts'
+import React, {useState} from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {colors} from '../../../utils/colors';
+import {fonts} from '../../../utils/fonts';
 
-const Input = ({label}) => {
-    return (
-        <View>
-            <Text style={styles.label}>{label}</Text>
-            <TextInput style={styles.input} />
-        </View>
-    )
-}
+const Input = ({label, value, onChangeText, secureTextEntry}) => {
+  const [border, setBorder] = useState(colors.border);
+  const onFocusForm = () => {
+    setBorder(colors.tertiary);
+  };
+  const onBlurForm = () => {
+    setBorder(colors.border);
+  };
 
-export default Input
+  return (
+    <View>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        style={styles.input(border)}
+        onFocus={onFocusForm}
+        onBlur={onBlurForm}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+      />
+    </View>
+  );
+};
+
+export default Input;
 
 const styles = StyleSheet.create({
-    input: {
-        borderWidth: 1,
-        borderColor: colors.border,
-        borderRadius: 10,
-        padding: 12
-    },
+  input: border => ({
+    borderWidth: 1,
+    borderColor: border,
+    borderRadius: 10,
+    padding: 12,
+    color: colors.black
+  }),
 
-    label: {
-        fontSize: 16,
-        color: colors.text.secondary,
-        marginBottom: 6,
-        fontFamily: fonts.primary[400]
-    }
-})
+  label: {
+    fontSize: 16,
+    color: colors.text.secondary,
+    marginBottom: 6,
+    fontFamily: fonts.primary[400],
+  },
+});
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Button, Gap, Header, Input } from '../../components/';
-import { colors } from '../../utils/colors';
+import React, {useState} from 'react';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import {Button, Gap, Header, Input} from '../../components/';
+import {colors, useForm} from '../../utils/';
 
 const SignUp = ({navigation}) => {
+  const [fullName, setFullName] = useState('');
+  const [job, setJob] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [form, setForm] = useForm({
+    fullName: '',
+    job: '',
+    email: '',
+    password: '',
+  });
+
+  const onContinue = () => {
+    console.log(form);
+    //navigation.navigate('UploadPhoto')
+  };
+
   return (
     <View style={styles.page}>
-      <Header title="Daftar Akun" onPress={() => navigation.goBack()}/>
-      <View style={styles.content}>
-        <Input label="Full Name" />
+      <Header title="Daftar Akun" onPress={() => navigation.goBack()} />
+      <ScrollView style={styles.content}>
+        <Input
+          label="Full Name"
+          value={form.fullName}
+          onChangeText={value => setForm('fullName', value)}
+        />
         <Gap height={24} />
-        <Input label="Pekerjaan" />
+        <Input
+          label="Pekerjaan"
+          value={form.job}
+          onChangeText={value => setForm('job', value)}
+        />
         <Gap height={24} />
-        <Input label="Email Address" />
+        <Input
+          label="Email Address"
+          value={form.email}
+          onChangeText={value => setForm('email', value)}
+        />
         <Gap height={24} />
-        <Input label="Password" />
+        <Input
+          label="Password"
+          value={form.password}
+          onChangeText={value => setForm('password', value)}
+          secureTextEntry={true}
+        />
         <Gap height={40} />
-        <Button title="Continue" onPress={() => navigation.navigate('UploadPhoto')}/>
-      </View>
+        <Button title="Continue" onPress={onContinue} />
+      </ScrollView>
     </View>
   );
 };
@@ -27,11 +61,11 @@ export default SignUp;
 const styles = StyleSheet.create({
   page: {
     backgroundColor: colors.white,
-    flex: 1
+    flex: 1,
   },
 
   content: {
     padding: 40,
     paddingTop: 0,
-  }
+  },
 });
