9b37a20ad4b7b685077683889c372e5fd1f7d22a
Haganta Brema Bangun <hagantabangun@gmail.com>
2021-09-21 22:12:54 +0700

    added login session

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -3,11 +3,22 @@ import {StyleSheet, Text, View} from 'react-native';
 import {ILLogo} from '../../assets';
 import {colors} from '../../utils/colors';
 import {fonts} from '../../utils/fonts';
+import {getAuth, onAuthStateChanged} from 'firebase/auth';
 
 const Splash = ({navigation}) => {
   useEffect(() => {
     setTimeout(() => {
-      navigation.replace('GetStarted');
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // user login
+          console.log('active user: ', user);
+          navigation.replace('MainApp');
+        } else {
+          // user logout
+          navigation.replace('GetStarted');
+        }
+      });
     }, 2000);
   }, [navigation]);
 
